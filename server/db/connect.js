5a21f0ef2a1c23d6154d05ea92fd883ef431ab89
Unknown <parjoona@hotmail.com>
2017-12-26 22:18:20 +0100

    Add DB

--- server/db/connect.js
+++ server/db/connect.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 // Makes our connection Promise based
 mongoose.Promise = global.Promise;
 // Connection
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+// Connecting to other DB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = mongoose;
