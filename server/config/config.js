61c683d9d075376632716befcaab205959147722
Unknown <parjoona@hotmail.com>
2017-12-31 12:49:03 +0100

    Changed to local variables
    PS : This is a test project

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,15 @@
 const env = process.env.NODE_ENV || 'development';
 
-console.log(env);
-
 // Checkar om vi är i development eller test
 // sätter till test om vi använder test-watch
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
-}
+
+if (env === 'develpment' || env === 'test') {
+  let config = require('./config.json');
+  let envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env(key) = envConfig[key];
+  });
+};
+
+console.log(`Enviroment: ${env}`);
